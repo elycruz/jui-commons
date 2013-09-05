d382ce1ee754509593eec3c346d47a6dd6d40050
Ely De La Cruz <elycruz@elycruz.com>
2013-09-05 16:18:52 -0400

    Adding in 'get element' function from original idea for further elaboration'

--- src/jui/jquery.juiBase.js
+++ src/jui/jquery.juiBase.js
@@ -1,4 +1,14 @@
 /**
+ * Static jui base fetch modes
+ */
+$.widget('jui.juiBaseFetchModes', {
+	options: {
+		createMode: 'create-mode',
+		fetchMode: 'fetch-mode'
+	}
+});
+
+/**
  * Created with JetBrains WebStorm.
  * User: ElyDeLaCruz
  * Date: 9/1/13
@@ -6,6 +16,60 @@
  * To change this template use File | Settings | File Templates.
  */
 $.widget('jui.juiBase', {
+	/**
+	 * Fetches an Element using a config hash section
+	 * @param config Config section from the options hash
+	 * @return mixed null || jquery
+	 * @throws Error
+	 */
+//	_getInternalElement: function (config) {
+//		var ops = this.options,
+//			appendToSection,
+//			elm = this.element;
+//
+//		// Element already exists
+//		// Return Element
+//		if (!empty(config.elm) && !empty(config.elm.length)) {
+//			return config.elm;
+//		}
+//
+//		// Selecte element
+//		if (typeof config.selector === 'string' && config.fetch) {
+//			config.elm = $(config.selector, elm);
+//		}
+//
+//		// Create element
+//		if (typeof config.html === 'string' && config.create) {
+//			config.elm = $(config.html);
+//			if (!empty(config.attribs)) {
+//				config.elm.attr(config.attribs);
+//			}
+//			config.createdInternally = true;
+//			if (config.appendToConfigSection) {
+//				appendToSection = ops[config.appendToConfigSection] || null;
+//				if (appendToSection.elm.length) {
+//					appendToSection.elm.append(config.elm);
+//				}
+//				else {
+//					throw new Error('Unable to append created element ' +
+//						'in "saks.scrollbar->getInternalElement"');
+//				}
+//			}
+//			else {
+//				this.element.append(config.elm);
+//			}
+//		}
+//		else if (config.elm !== null && !config.elm.length) {
+//			throw new Error('Unable to fetch an element for config: ' +
+//				config + 'in "saks.scrollbar->getInternalElement"');
+//		}
+//
+//		// If we made it out of the jungle successfully
+//		// Return the Element
+//		return config.elm;
+//	},
+
+
     _getElementFromConfigSection:function (configSection) {
         var plugin = this,
             ops = plugin.options,
@@ -18,7 +82,7 @@ $.widget('jui.juiBase', {
 
         // If config is a function
         if (typeof config === 'function') {
-            config = config();
+            config = config.apply(plugin);
         }
 
         // If config is empty return
